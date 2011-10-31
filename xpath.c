82fe084c750cb2e6c839ec8b6aad5e2bb87c14dd
Selim Gurun <sgurun@google.com>
2011-10-31 14:51:50 -0700

    Fix XPath freeing error.
    	Bug: 5533654
    
    Applied a patch from Chrome source code to prevent potential
    multiple freeing and memory corruption in XPath. This patch
    will become unnecessary once we pull in new libxml2.
    The Chrome changes are here:
    http://codereview.chromium.org/5196003
    http://codereview.chromium.org/7508039
    
    Change-Id: I0dd573e2f8e3cfbd1290735e68e44ebb13597482
    Signed-off-by: Selim Gurun <sgurun@google.com>

--- xpath.c
+++ xpath.c
@@ -11736,11 +11736,16 @@ xmlXPathCompOpEvalPositionalPredicate(xmlXPathParserContextPtr ctxt,
 
 	    if ((ctxt->error != XPATH_EXPRESSION_OK) || (res == -1)) {
 	        xmlXPathObjectPtr tmp;
-		/* pop the result */
-		tmp = valuePop(ctxt);
-		xmlXPathReleaseObject(xpctxt, tmp);
-		/* then pop off contextObj, which will be freed later */
-		valuePop(ctxt);
+	        /* pop the result if any */
+	        tmp = valuePop(ctxt);
+	        while (tmp != contextObj) {
+	            /*
+	             * Free up the result
+	             * then pop off contextObj, which will be freed later
+	             */
+	             xmlXPathReleaseObject(xpctxt, tmp);
+	             tmp = valuePop(ctxt);
+	        }
 		goto evaluation_error;
 	    }
 
